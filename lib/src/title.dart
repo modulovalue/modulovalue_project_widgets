a31b4066226bfdf3db3cbfd80241daf0184a1ad0
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-10-30 16:51:22 +0100

    added a no GitHub title

--- lib/src/title.dart
+++ lib/src/title.dart
@@ -5,6 +5,30 @@ import 'package:flutter/material.dart';
 
 Iterable<Widget> modulovalueTitle(String title, String repo) {
   return [
+    ...modulovalueTitleNoGitHub(title),
+    if (repo != null) //
+      ...[
+      SizedBox(height: 4.0),
+      GestureDetector(
+        onTap: () => js.context
+            .callMethod("open", ["https://github.com/modulovalue/$repo"]),
+        child: Text(
+          "GitHub",
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: Colors.grey[700],
+            fontSize: 12.0,
+            decoration: TextDecoration.underline,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    ],
+  ];
+}
+
+Iterable<Widget> modulovalueTitleNoGitHub(String title) {
+  return [
     Text(
       title,
       style: TextStyle(
@@ -19,21 +43,6 @@ Iterable<Widget> modulovalueTitle(String title, String repo) {
           js.context.callMethod("open", ["https://twitter.com/modulovalue"]),
       child: Text(
         "by @modulovalue",
-        style: TextStyle(
-          fontWeight: FontWeight.w700,
-          color: Colors.grey[700],
-          fontSize: 12.0,
-          decoration: TextDecoration.underline,
-        ),
-        textAlign: TextAlign.center,
-      ),
-    ),
-    SizedBox(height: 4.0),
-    GestureDetector(
-      onTap: () => js.context
-          .callMethod("open", ["https://github.com/modulovalue/$repo"]),
-      child: Text(
-        "GitHub",
         style: TextStyle(
           fontWeight: FontWeight.w700,
           color: Colors.grey[700],
